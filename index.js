faf8c4ec0496a8a541de72bf3f8e169b9d35071f
kanizfatiha <87264831+kanizfatiha@users.noreply.github.com>
2022-10-23 12:52:18 +0600

    Connection with mongodB

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello world")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO_URL
+).then(()=>console.log("DB connection successful")).catch((err)=>{
+    console.log(err);
+});
+
+app.listen(5000, ()=>{
+    console.log("Backend server is running");
+});
